18973e86d610a294ed90e16dd3a3194d37b56a37
rodrigues-leo97 <leonardodrodriguess@gmail.com>
2022-03-09 00:00:50 -0300

    MovieCard

--- frontend/src/pages/Form/index.tsx
+++ frontend/src/pages/Form/index.tsx
@@ -1,3 +1,5 @@
+import './styles.css'
+
 function Form() {
 
     const movie = {
--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,8 +1,34 @@
+import MovieCard from "components/MovieCard";
+import MovieScore from "components/MovieScore";
 import Pagination from "components/Pagination";
 
 function Listing() {
     return (
-        <Pagination />
+        <>
+            <Pagination />
+            
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                        {/* Chama o MovieScore pois o MovieCard pois dentro dele tem o MovieScore que tem o MovieStar já está dentro dele*/}
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3"> {/* mb -> margin-bottom*/} 
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                </div> {/* row = classe bootstrap*/}
+            </div>
+        </> //nome é fragment, não muda nada na prática
+
     );
 }
 
