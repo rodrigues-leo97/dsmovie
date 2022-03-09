100f89a970fc431ece9cf4b546781e42cd1a483e
rodrigues-leo97 <leonardodrodriguess@gmail.com>
2022-03-08 22:51:18 -0300

    Pagination layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -17,7 +17,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Listing />} />
         <Route path="/form">
-          <Route path=":movieId" element={<Form />} />
+          <Route path=":movieId" element={<Form />} /> {/* uma subrota onde terá o id*/}
         </Route>
       </Routes>
     </BrowserRouter>
