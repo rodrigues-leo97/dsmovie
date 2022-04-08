2b3c764ebf1deb049c035df98f3c8b953b91ae3f
rodrigues-leo97 <leonardodrodriguess@gmail.com>
2022-04-07 22:29:00 -0300

    useState, useEffect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,17 +1,28 @@
 import axios from "axios";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
+import { useEffect, useState } from "react";
+import { MoviePage } from "types/movie";
 import { BASE_URL } from "utils/requests";
 
 function Listing() {
 
-    axios.get(`${BASE_URL}/movies?size=12&page=0`)
+    //useState
+    const [pageNumber, setPageNumber] = useState(0); //boa prática inicar ele com valor, o valor inicial pageNumber será o zero por conta da página zero
+    useEffect(() => {
+        axios.get(`${BASE_URL}/movies?size=12&page=0`)
             .then(response => {
-                console.log(response.data)
+                const data = response.data as MoviePage //para reconhecer o response.data como o tipo MoviePage
+                console.log('GET response.data', data)
+                setPageNumber(data.number) //irá imprimir a página, e isso vem da API
+                //OBS: se eu trocar na URL o page para 1, ele irá trocar a página também que irá exibir na tela
             });
+    }, []) //recebe 2 argumentos, uma função para executar e o segundos é uma lista de objetos que irá observar
             
     return (
         <>
+
+        <p>{ pageNumber }</p>
             <Pagination />
             
             <div className="container">
