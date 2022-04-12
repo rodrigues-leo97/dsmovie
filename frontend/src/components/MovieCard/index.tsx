de3ecdd485a21316984500ec3f3704a88e724cd3
rodrigues-leo97 <leonardodrodriguess@gmail.com>
2022-04-11 23:35:43 -0300

    Show score

--- frontend/src/components/MovieCard/index.tsx
+++ frontend/src/components/MovieCard/index.tsx
@@ -7,16 +7,17 @@ type Props = {
 
     movie: Movie; //neste caso é só um objeto do tipo Movie
     //sendo então o tipo de dado que minha função irá receber
+
 }
 
-function MovieCard( { movie }: Props ) { //passo um obj movie do tipo Props(tipo criado acima)
+function MovieCard( { movie  }: Props ) { //passo um obj movie do tipo Props(tipo criado acima)
 
     return (
         <div>
             <img className="dsmovie-movie-card-image" src={ movie.image } alt={ movie.title } />
             <div className="dsmovie-card-bottom-container">
                 <h3>{ movie.title }</h3>
-                <MovieScore />
+                <MovieScore score={movie.score} count={movie.count}/>
 
                 <Link to={`/form/${ movie.id }`} >
                     <div className="btn btn-primary dsmovie-btn">Avaliar</div>
